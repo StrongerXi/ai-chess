df6d21974af4eeec0bbc72858f1a1cff0b7d0f27
Ryan <ryan.guo99@gmail.com>
2020-08-11 18:08:16 -0400

    Support view selection via command line arguments

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,47 @@ import aiChess.model.ChessGameModel;
 
 import java.io.InputStreamReader;
 
-import javax.swing.JFrame;
-
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 
 /**
  * Entry class of the application.
  */
 public class Main {
 
-  public static void main(String[] args) {
-    Readable input = new InputStreamReader(System.in);
-    Appendable output = System.out;
+  // Command line arguments for the application
+  private static class Args {
+    @Parameter(names = "--view", description = "The type of view to be used. { text, gui }")
+    public String view = "gui";
+    @Parameter(names = "--help", description = "Print out user instructions")
+    public boolean help = false;
+  }
+
+  public static void main(String[] argv) {
+    var args = new Args();
+    var parser = JCommander.newBuilder().addObject(args).build();
+    parser.parse(argv);
+    if (args.help) {
+      parser.usage();
+      return;
+    }
 
     ChessGameModel model = new ChessGameModel();
-    //ChessView view = new TextualView(input, output);
-    ChessView view = new SwingView(model);
+    ChessView view;
+    switch (args.view) {
+      case "text": {
+        Readable input = new InputStreamReader(System.in);
+        Appendable output = System.out;
+        view = new TextualView(input, output);
+        break;
+      }
+      case "gui" : {
+        view = new SwingView(model);
+        break;
+      }
+      default:
+        throw new IllegalArgumentException("Unsupported view: " + args.view);
+    }
     ChessController control = new ChessController(model, view);
   }
 }
